18effb79c7224c1552e34d665a5f0a232a3d8d2d
PaulGureghian <paulgureghian@att.net>
2020-08-17 13:21:29 -0700

    Added 'main.go' source code file

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/pluralsight/webservice/controllers"
+)
+
+func main() {
+
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
+
+}
