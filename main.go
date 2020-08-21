1fe74e7f445beda66406ba8c45df1d1a725d3680
PaulGureghian <paulgureghian@att.net>
2020-08-21 13:00:44 -0700

    Made minor edits

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ import (
 func main() {
 
 	fmt.Println("Web service starting")
+	fmt.Println("Web service started on localhost:3000")
 
 	controllers.RegisterControllers()
 	http.ListenAndServe(":3000", nil)
 
-	fmt.Println("Web service started on localhost:3000")
 }
